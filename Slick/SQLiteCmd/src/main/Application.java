18a20ba1d4d6a59ff548a3566468948ae8d3ec9f
Afnarel <afnarel@mailoo.org>
2011-04-07 09:56:55 +0200

    Ajout du projet SQLiteCmd pour l'interaction avec la base de données.

---
+++ Slick/SQLiteCmd/src/main/Application.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import bdd.DBInteractor;
+
+public class Application {
+
+	private BufferedReader reader;
+	private DBInteractor db;
+	
+	public Application() {
+		
+	}
+	
+	private void createQuestionsFromFile(String filename) {
+		
+	}
+}
